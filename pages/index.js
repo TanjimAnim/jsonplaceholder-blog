c578642a011eb59edc231b97b4fa64a1386d806d
TanjimAnim <tanjimanim007@gmail.com>
2022-11-22 21:13:30 +0600

    edit:console.log removed

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home({ data, userData, commentData }) {
   );
   const deletePost = (id) => {
     setPostData(postData.filter((elem) => elem.id !== id));
-    console.log(postData);
   };
   return (
     <>
