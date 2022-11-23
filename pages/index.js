5a353f3ae0b0a04e1b8e5d5cf0a34493eb1e8b74
TanjimAnim <tanjimanim007@gmail.com>
2022-11-23 21:10:28 +0600

    feat:implmented layout design

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,131 @@
 import axios from "axios";
 import slugify from "slugify";
 import Link from "next/link";
-import { useState } from "react";
-import { Button } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
-import { Typography } from "antd";
+import { useState, Suspense } from "react";
+import { Box, Text, Heading, Button, useToast } from "@chakra-ui/react";
+//import icons
+import { DeleteIcon, ChatIcon } from "@chakra-ui/icons";
+import { FaQuoteLeft } from "react-icons/fa";
+
+import Loader from "../src/components/loader";
 
 export default function Home({ data, userData, commentData }) {
-  const { Title } = Typography;
+  const toast = useToast();
   const [postData, setPostData] = useState(
     data.filter((elem) => elem.id <= 20)
   );
   const deletePost = (id) => {
+    console.log(
+      toast({
+        title: "Post Deleted",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      })
+    );
     setPostData(postData.filter((elem) => elem.id !== id));
   };
+
   return (
     <>
-      <Title>Blog</Title>
-      {postData.map((item) => {
-        return (
-          <div key={item.id}>
-            <div>
-              <span style={{ fontWeight: "700" }}>title:</span>{" "}
-              <Link href={`/posts/${item.id}/${slugify(item.title)}`}>
-                {item.title}
-              </Link>
-            </div>
-            <div>
-              <span style={{ fontWeight: "700" }}>author:</span>
-              {userData.map((user) => {
-                if (user.id === item.userId) {
-                  return <p key={user.id}>{user.name}</p>;
-                }
-              })}
-            </div>
-            <div>
-              <span style={{ fontWeight: "700" }}>comments:</span>
-              {commentData.filter((elem) => elem.postId === item.id).length}
-            </div>
-            <div>
-              <Button
-                onClick={() => {
-                  deletePost(item.id);
+      <Suspense fallback={<Loader />}>
+        <Box display="flex" justifyContent="center" marginY="20px">
+          <Heading as="h1" fontSize="50px" color="#2e8c81">
+            Blog
+          </Heading>
+        </Box>
+        <Box
+          padding="1rem"
+          marginX="1rem"
+          display="flex"
+          flexWrap="wrap"
+          justifyContent="center"
+          cursor="pointer"
+        >
+          {postData.map((item) => {
+            return (
+              <Box
+                key={item.id}
+                margin="10px"
+                padding="1rem"
+                width="500px"
+                border="1px solid aliceblue"
+                borderRadius="10px"
+                boxShadow="5px 5px powderblue"
+                transition="border 0.3s ease-in,box-shadow 0.3s ease-in"
+                _hover={{
+                  border: "1px solid powderblue",
+                  boxShadow: "10px 10px powderblue",
                 }}
-                danger
+                display="flex"
+                flexDirection="column"
+                backgroundColor="#fefbd8"
               >
-                Delete
-                <DeleteOutlined />
-              </Button>
-            </div>
-          </div>
-        );
-      })}
+                <Box marginY="10px" minHeight="165px">
+                  <Link href={`/posts/${item.id}/${slugify(item.title)}`}>
+                    <Text color="#618685" fontWeight={700} fontSize="30px">
+                      <FaQuoteLeft />
+                      {item.title}
+                    </Text>
+                  </Link>
+                </Box>
+                <Box marginY="10px">
+                  <Box>
+                    {userData.map((user) => {
+                      if (user.id === item.userId) {
+                        return (
+                          <Text
+                            key={user.id}
+                            fontSize="20px"
+                            color="#618685"
+                            fontWeight={700}
+                          >
+                            <span style={{ color: "#80ced6" }}>Author: </span>
+                            {user.name}
+                          </Text>
+                        );
+                      }
+                    })}
+                  </Box>
+                  <Box marginY="10px">
+                    <Text fontSize="20px" color="#618685" fontWeight={700}>
+                      <span style={{ color: "#80ced6" }}>
+                        <ChatIcon /> No of Comments :
+                      </span>{" "}
+                      {
+                        commentData.filter((elem) => elem.postId === item.id)
+                          .length
+                      }
+                    </Text>
+                  </Box>
+                  <Box marginY="10px">
+                    <Button
+                      background="#f4f4f4"
+                      color="#80ced6"
+                      cursor="pointer"
+                      borderRadius="5%"
+                      border="none"
+                      padding="1rem"
+                      fontSize="20px"
+                      onClick={() => {
+                        deletePost(item.id);
+                      }}
+                      _hover={{
+                        background: "#f0110a",
+                        color: "white",
+                      }}
+                      gap="5px"
+                    >
+                      Delete
+                      <DeleteIcon />
+                    </Button>
+                  </Box>
+                </Box>
+              </Box>
+            );
+          })}
+        </Box>
+      </Suspense>
     </>
   );
 }
