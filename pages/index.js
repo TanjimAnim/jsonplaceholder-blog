f9a10de62adc0a85e4a2d5ffa573ae5d035c0796
TanjimAnim <tanjimanim007@gmail.com>
2022-11-24 18:56:47 +0600

    fix:removed loader

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import slugify from "slugify";
 import Link from "next/link";
-import { useState, Suspense } from "react";
+import { useState } from "react";
 import { Box, Text, Heading, Button, useToast } from "@chakra-ui/react";
 //import icons
 import { DeleteIcon, ChatIcon } from "@chakra-ui/icons";
 import { FaQuoteLeft } from "react-icons/fa";
 
-import Loader from "../src/components/loader";
+//import Loader from "../src/components/loader";
 
 export default function Home({ data, userData, commentData }) {
   const toast = useToast();
@@ -26,105 +26,103 @@ export default function Home({ data, userData, commentData }) {
 
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Box display="flex" justifyContent="center" marginY="20px">
-          <Heading as="h1" fontSize="50px" color="#2e8c81">
-            Blog
-          </Heading>
-        </Box>
-        <Box
-          padding="1rem"
-          marginX="1rem"
-          display="flex"
-          flexWrap="wrap"
-          justifyContent="center"
-          cursor="pointer"
-        >
-          {postData.map((item) => {
-            return (
-              <Box
-                key={item.id}
-                margin="10px"
-                padding="1rem"
-                width="500px"
-                border="1px solid aliceblue"
-                borderRadius="10px"
-                boxShadow="5px 5px powderblue"
-                transition="border 0.3s ease-in,box-shadow 0.3s ease-in"
-                _hover={{
-                  border: "1px solid powderblue",
-                  boxShadow: "10px 10px powderblue",
-                }}
-                display="flex"
-                flexDirection="column"
-                backgroundColor="#fefbd8"
-              >
-                <Box marginY="10px" minHeight="165px">
-                  <Link href={`/posts/${item.id}/${slugify(item.title)}`}>
-                    <Text color="#618685" fontWeight={700} fontSize="30px">
-                      <FaQuoteLeft />
-                      {item.title}
-                    </Text>
-                  </Link>
+      <Box display="flex" justifyContent="center" marginY="20px">
+        <Heading as="h1" fontSize="50px" color="#2e8c81">
+          Blog
+        </Heading>
+      </Box>
+      <Box
+        padding="1rem"
+        marginX="1rem"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="center"
+        cursor="pointer"
+      >
+        {postData.map((item) => {
+          return (
+            <Box
+              key={item.id}
+              margin="10px"
+              padding="1rem"
+              width="500px"
+              border="1px solid aliceblue"
+              borderRadius="10px"
+              boxShadow="5px 5px powderblue"
+              transition="border 0.3s ease-in,box-shadow 0.3s ease-in"
+              _hover={{
+                border: "1px solid powderblue",
+                boxShadow: "10px 10px powderblue",
+              }}
+              display="flex"
+              flexDirection="column"
+              backgroundColor="#fefbd8"
+            >
+              <Box marginY="10px" minHeight="165px">
+                <Link href={`/posts/${item.id}/${slugify(item.title)}`}>
+                  <Text color="#618685" fontWeight={700} fontSize="30px">
+                    <FaQuoteLeft />
+                    {item.title}
+                  </Text>
+                </Link>
+              </Box>
+              <Box marginY="10px">
+                <Box>
+                  {userData.map((user) => {
+                    if (user.id === item.userId) {
+                      return (
+                        <Text
+                          key={user.id}
+                          fontSize="20px"
+                          color="#618685"
+                          fontWeight={700}
+                        >
+                          <span style={{ color: "#80ced6" }}>Author: </span>
+                          {user.name}
+                        </Text>
+                      );
+                    }
+                  })}
                 </Box>
                 <Box marginY="10px">
-                  <Box>
-                    {userData.map((user) => {
-                      if (user.id === item.userId) {
-                        return (
-                          <Text
-                            key={user.id}
-                            fontSize="20px"
-                            color="#618685"
-                            fontWeight={700}
-                          >
-                            <span style={{ color: "#80ced6" }}>Author: </span>
-                            {user.name}
-                          </Text>
-                        );
-                      }
-                    })}
-                  </Box>
-                  <Box marginY="10px">
-                    <Text fontSize="20px" color="#618685" fontWeight={700}>
-                      <span style={{ color: "#80ced6" }}>
-                        <ChatIcon /> No of Comments :
-                      </span>{" "}
-                      {
-                        commentData.comments.filter(
-                          (elem) => elem.postId === item.id
-                        ).length
-                      }
-                    </Text>
-                  </Box>
-                  <Box marginY="10px">
-                    <Button
-                      background="#f4f4f4"
-                      color="#80ced6"
-                      cursor="pointer"
-                      borderRadius="5%"
-                      border="none"
-                      padding="1rem"
-                      fontSize="20px"
-                      onClick={() => {
-                        deletePost(item.id);
-                      }}
-                      _hover={{
-                        background: "#f0110a",
-                        color: "white",
-                      }}
-                      gap="5px"
-                    >
-                      Delete
-                      <DeleteIcon />
-                    </Button>
-                  </Box>
+                  <Text fontSize="20px" color="#618685" fontWeight={700}>
+                    <span style={{ color: "#80ced6" }}>
+                      <ChatIcon /> No of Comments :
+                    </span>{" "}
+                    {
+                      commentData.comments.filter(
+                        (elem) => elem.postId === item.id
+                      ).length
+                    }
+                  </Text>
+                </Box>
+                <Box marginY="10px">
+                  <Button
+                    background="#f4f4f4"
+                    color="#80ced6"
+                    cursor="pointer"
+                    borderRadius="5%"
+                    border="none"
+                    padding="1rem"
+                    fontSize="20px"
+                    onClick={() => {
+                      deletePost(item.id);
+                    }}
+                    _hover={{
+                      background: "#f0110a",
+                      color: "white",
+                    }}
+                    gap="5px"
+                  >
+                    Delete
+                    <DeleteIcon />
+                  </Button>
                 </Box>
               </Box>
-            );
-          })}
-        </Box>
-      </Suspense>
+            </Box>
+          );
+        })}
+      </Box>
     </>
   );
 }
--- pages/posts/[[...posts]].js
+++ pages/posts/[[...posts]].js
@@ -3,111 +3,104 @@ import axios from "axios";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { ChatIcon } from "@chakra-ui/icons";
 import Link from "next/link";
-import { Suspense } from "react";
-import Loader from "../../src/components/loader";
+//import { Suspense } from "react";
+//import Loader from "../../src/components/loader";
 export default function Post({ postData, userData, commentData }) {
   const router = useRouter();
   const { posts } = router.query;
 
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Box display="flex" justifyContent="center" marginY="20px">
-          <Link href="/">
-            <Heading as="h1" fontSize="50px" color="#2e8c81">
-              Blog
-            </Heading>
-          </Link>
-        </Box>
-        <Box
-          background="#fefbd8"
-          padding="1rem"
-          marginX="1rem"
-          minHeight="80vh"
-        >
-          {postData.posts.map((item) => {
-            if (item.id === parseInt(posts[0])) {
-              return (
-                <Box key={item.id}>
-                  <Box>
-                    {userData.map((user) => {
-                      if (user.id === item.userId) {
-                        return (
+      <Box display="flex" justifyContent="center" marginY="20px">
+        <Link href="/">
+          <Heading as="h1" fontSize="50px" color="#2e8c81">
+            Blog
+          </Heading>
+        </Link>
+      </Box>
+      <Box background="#fefbd8" padding="1rem" marginX="1rem" minHeight="80vh">
+        {postData.posts.map((item) => {
+          if (item.id === parseInt(posts[0])) {
+            return (
+              <Box key={item.id}>
+                <Box>
+                  {userData.map((user) => {
+                    if (user.id === item.userId) {
+                      return (
+                        <Text
+                          key={user.id}
+                          fontSize="20px"
+                          color="#618685"
+                          fontWeight={700}
+                        >
+                          <span
+                            style={{
+                              color: "#80ced6",
+                              fontWeight: "600",
+                              fontSize: "16px",
+                            }}
+                          >
+                            Posted By
+                          </span>{" "}
+                          {user.name}
+                        </Text>
+                      );
+                    }
+                  })}
+                </Box>
+                <Box marginY="10px">
+                  <Text color="#618685" fontWeight={700} fontSize="30px">
+                    {item.title}
+                  </Text>
+                </Box>
+
+                <Box marginY="10px">
+                  <Text color="#80ced6" fontSize="24px" textAlign="justify">
+                    {item.body}
+                  </Text>
+                </Box>
+                <Box marginY="10px">
+                  <Text fontSize="20px" color="lightblue" fontWeight={700}>
+                    <span style={{ color: "#71b8bf" }}>
+                      Comments <ChatIcon />
+                    </span>{" "}
+                    {
+                      commentData.comments.filter(
+                        (elem) => elem.postId === item.id
+                      ).length
+                    }
+                  </Text>
+                </Box>
+                <Box>
+                  {commentData.comments.map((comment) => {
+                    if (comment.postId === parseInt(posts[0]))
+                      return (
+                        <Box
+                          key={comment.id}
+                          borderLeft="1px solid #71b8bf"
+                          marginY="10px"
+                          padding="1rem"
+                        >
+                          <Text color="#80ced6" fontSize="18px">
+                            {comment.email}
+                          </Text>
+
                           <Text
-                            key={user.id}
-                            fontSize="20px"
-                            color="#618685"
+                            color="#71b8bf"
+                            fontSize="18px"
                             fontWeight={700}
                           >
-                            <span
-                              style={{
-                                color: "#80ced6",
-                                fontWeight: "600",
-                                fontSize: "16px",
-                              }}
-                            >
-                              Posted By
-                            </span>{" "}
-                            {user.name}
+                            {comment.body}
                           </Text>
-                        );
-                      }
-                    })}
-                  </Box>
-                  <Box marginY="10px">
-                    <Text color="#618685" fontWeight={700} fontSize="30px">
-                      {item.title}
-                    </Text>
-                  </Box>
-
-                  <Box marginY="10px">
-                    <Text color="#80ced6" fontSize="24px" textAlign="justify">
-                      {item.body}
-                    </Text>
-                  </Box>
-                  <Box marginY="10px">
-                    <Text fontSize="20px" color="lightblue" fontWeight={700}>
-                      <span style={{ color: "#71b8bf" }}>
-                        Comments <ChatIcon />
-                      </span>{" "}
-                      {
-                        commentData.comments.filter(
-                          (elem) => elem.postId === item.id
-                        ).length
-                      }
-                    </Text>
-                  </Box>
-                  <Box>
-                    {commentData.comments.map((comment) => {
-                      if (comment.postId === parseInt(posts[0]))
-                        return (
-                          <Box
-                            key={comment.id}
-                            borderLeft="1px solid #71b8bf"
-                            marginY="10px"
-                            padding="1rem"
-                          >
-                            <Text color="#80ced6" fontSize="18px">
-                              {comment.email}
-                            </Text>
-
-                            <Text
-                              color="#71b8bf"
-                              fontSize="18px"
-                              fontWeight={700}
-                            >
-                              {comment.body}
-                            </Text>
-                          </Box>
-                        );
-                    })}
-                  </Box>
+                        </Box>
+                      );
+                  })}
                 </Box>
-              );
-            }
-          })}
-        </Box>
-      </Suspense>
+              </Box>
+            );
+          }
+        })}
+      </Box>
     </>
   );
 }
